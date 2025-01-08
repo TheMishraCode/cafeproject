3db9475f9be70c62513c961237db4495111e47f3
BBnomnom <lovevkmagical@gmail.com>
2025-01-08 17:49:19 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Hero from './components/Hero/Hero.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router'
 import About from './components/About/About.jsx'
 import OurServices from './components/OurServices/OurServices.jsx'
 import Contact from './components/Contact/Contact.jsx'
@@ -44,11 +44,27 @@ const router = createBrowserRouter([
   ]
 }
 ])
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<App/>}>
+//      <Route path='' element={<Hero/>}/>
+//      <Route path='about' element={<About/>}/>
+//      <Route path='contact' element={<Contact/>}/>
+//      <Route path='menu' element={<Toplist/>}/>
+//      <Route path='cart' element={<Cart/>}/>
+//      <Route path='ourservices' element={<OurServices/>}/>
+//     </Route>
+//   )
+// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartProvider>  
     <RouterProvider router={router}/>
     </CartProvider>
-  </StrictMode>,
+  </StrictMode>
 )
+
+
+ 
+
